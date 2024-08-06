441987d01feecc916d2e0e9f7f2e8722d338ef2c
hithirani <hit.devoteeinfotech@gmail.com>
2024-08-06 12:21:24 +0530

    add rato and open side

--- index.js
+++ index.js
@@ -16,120 +16,305 @@ let inputStreet1 = document.querySelector(".input-street-1");
 let inputStreet2 = document.querySelector(".input-street-2");
 let inputStreet3 = document.querySelector(".input-street-3");
 let inputStreet4 = document.querySelector(".input-street-4");
-
+let extraTimeCountDown;
 let stop = false;
-console.log(allLight1);
 
-let demo = 100;
-console.log((demo / 100) * 40);
+let arrow1 = document.querySelectorAll(".street-1 .arrow");
+let arrow2 = document.querySelectorAll(".street-2 .arrow");
+let arrow3 = document.querySelectorAll(".street-3 .arrow");
+let arrow4 = document.querySelectorAll(".street-4 .arrow");
+
+function extraTime(timer) {
+    let time = parseInt(totalTime.value);
+
+    let extraTime = time - Math.floor(time / 4) + 3;
+    extraTimeCountDown = setInterval(() => {
+        if (extraTime > -1) {
+            timer.textContent = extraTime;
+            extraTime--;
+            if (extraTime == 0) {
+                timer.textContent = "Go";
+            }
+        }
+    }, 1000);
+}
 
 async function timeDivider() {
-    let ratioCount = null;
-    let ratioInput = document.querySelectorAll(".ratio");
-    
-    ratioInput.forEach((value) => {
-        if (value.value != undefined && value.value != "") {
-            ratioCount += parseInt(value.value);
-        }
-    });
-    console.log(ratioCount);
-    
     let time = parseInt(totalTime.value);
-    console.log(time / 25);
-    let modifyCount1 = time / (100 * parseInt(inputStreet1.value));
-    let modifyCount2 = time / (100 * parseInt(inputStreet2.value));
-    let modifyCount3 = time / (100 * parseInt(inputStreet3.value));
-    let modifyCount4 = time / (100 * parseInt(inputStreet4.value));
-console.log(modifyCount1);
 
     if (isNaN(time) || time <= 0) {
         alert("Please enter a valid total time.");
         return;
     }
-    let count1 = modifyCount1 || Math.floor(time / 4);
-    let count2 = modifyCount2 || Math.floor(time / 4);
-    let count3 = modifyCount3 || Math.floor(time / 4);
-    let count4 = modifyCount4 || Math.floor(time / 4);
 
-    function setGreen(lights) {
+    let count1 = Math.floor(time / 4);
+    let count2 = Math.floor(time / 4);
+    let count3 = Math.floor(time / 4);
+    let count4 = Math.floor(time / 4);
+
+    function setGreen(lights, arrow) {
         lights.forEach((light) => {
             if (light.classList.contains("light-green")) {
                 light.classList.add("green");
             }
             light.classList.remove("red");
         });
+
+        if (arrow) {
+            arrow.forEach((value) => {
+                value.style.color = "white";
+            });
+        }
     }
 
-    function setRed(lights) {
+    function setRed(lights, arrow) {
         lights.forEach((light) => {
             if (light.classList.contains("light-red")) {
                 light.classList.add("red");
             }
             light.classList.remove("green");
         });
+
+        if (arrow) {
+            arrow.forEach((value) => {
+                if (!value.classList.contains("left")) {
+                    value.style.color = "rgb(20,20,20)";
+                }
+            });
+        }
+    }
+
+    timer1 = await setInterval(
+        () => {
+            if (count1 >= 1) {
+                setGreen(lights1, arrow1);
+                setRed(lights2, arrow2);
+                setRed(lights3, arrow3);
+                setRed(lights4, arrow4);
+                time1.textContent = count1;
+                count1--;
+            } else {
+                extraTime(time1);
+                setRed(lights1, arrow1);
+                time1.textContent = "stop";
+                clearInterval(timer1);
+                timer2 = setInterval(
+                    () => {
+                        if (count2 >= 1) {
+                            setGreen(lights2, arrow2);
+                            setRed(lights3, arrow3);
+                            setRed(lights4, arrow4);
+                            time2.textContent = count2;
+                            count2--;
+                        } else {
+                            time2.textContent = "stop";
+
+                            extraTime(time2);
+                            setRed(lights2, arrow2);
+                            clearInterval(timer2);
+
+                            timer3 = setInterval(() => {
+                                if (count3 >= 1) {
+                                    setGreen(lights3, arrow3);
+                                    setRed(lights4, arrow4);
+                                    time3.textContent = count3;
+                                    count3--;
+                                } else {
+                                    time3.textContent = "stop";
+
+                                    extraTime(time3);
+                                    setRed(lights3, arrow3);
+                                    clearInterval(timer3);
+
+                                    timer4 = setInterval(() => {
+                                        if (count4 >= 1) {
+                                            setGreen(lights4, arrow4);
+                                            time4.textContent = count4;
+                                            count4--;
+                                        } else {
+                                            time4.textContent = "stop";
+
+                                            extraTime(time4);
+                                            setRed(lights4, arrow4);
+                                            clearInterval(timer4);
+                                            if (
+                                                count1 == 0 &&
+                                                count2 == 0 &&
+                                                count3 == 0 &&
+                                                count4 == 0
+                                            ) {
+                                                timeDivider();
+                                            }
+                                        }
+                                    }, 1000);
+                                }
+                            }, 1000);
+                        }
+                    },
+                    1000,
+                    0
+                );
+            }
+        },
+        1000,
+        0
+    );
+}
+
+function extraTimeForRatio(timer, elapsedTime) {
+    let extraTime = elapsedTime + 3;
+
+    extraTimeCountDown = setInterval(() => {
+        if (extraTime > -1) {
+            timer.textContent = extraTime;
+            extraTime--;
+        }
+        if (extraTime == 0) {
+            timer.textContent = "Go";
+        }
+    }, 1000);
+}
+
+async function ratioWIseTimeDivider() {
+    let time = parseInt(totalTime.value);
+    let ratioCount = null;
+    let ratioInput = document.querySelectorAll(".ratio");
+    let blankInput = [];
+    let fillInput = [];
+
+    // ratioInput.forEach((value) => {
+    //     if (isNaN(value.value) || value.value <= 0) {
+    //         alert("Please enter a valid total time.");
+    //         return;
+    //     }
+    // })
+
+    ratioInput.forEach((value) => {
+        if (value.value != undefined && value.value != "") {
+            ratioCount += parseInt(value.value);
+            fillInput.push(value);
+        }
+        if (value.value == "") {
+            blankInput.push(value);
+        }
+    });
+    console.log(ratioCount);
+    console.log("fill", fillInput.length);
+    console.log("blank", blankInput.length);
+    let obtainTime = time / (100 / ratioCount);
+
+    let modifyCount1 =
+        time / (100 / parseInt(inputStreet1.value)) ||
+        Math.floor((time - obtainTime) / blankInput.length);
+    let modifyCount2 =
+        time / (100 / parseInt(inputStreet2.value)) ||
+        Math.floor((time - obtainTime) / blankInput.length);
+    let modifyCount3 =
+        time / (100 / parseInt(inputStreet3.value)) ||
+        Math.floor((time - obtainTime) / blankInput.length);
+    let modifyCount4 =
+        time / (100 / parseInt(inputStreet4.value)) ||
+        Math.floor((time - obtainTime) / blankInput.length);
+
+    let remainingTime1 = time - modifyCount1;
+    let remainingTime2 = time - modifyCount2;
+    let remainingTime3 = time - modifyCount3;
+    let remainingTime4 = time - modifyCount4;
+
+    if (isNaN(time) || time <= 0) {
+        alert("Please enter a valid total time.");
+        return;
+    }
+
+    function setGreen(lights, arrow) {
+        lights.forEach((light) => {
+            if (light.classList.contains("light-green")) {
+                light.classList.add("green");
+            }
+            light.classList.remove("red");
+        });
+
+        if (arrow) {
+            arrow.forEach((value) => {
+                value.style.color = "white";
+            });
+        }
+    }
+
+    function setRed(lights, arrow) {
+        lights.forEach((light) => {
+            if (light.classList.contains("light-red")) {
+                light.classList.add("red");
+            }
+            light.classList.remove("green");
+        });
+
+        if (arrow) {
+            arrow.forEach((value) => {
+                if (!value.classList.contains("left")) {
+                    value.style.color = "rgb(20,20,20)";
+                }
+            });
+        }
     }
 
     timer1 = await setInterval(() => {
-        if (count1 > 0) {
-            setGreen(lights1);
-            setRed(lights2);
-            setRed(lights3);
-            setRed(lights4);
-            time1.textContent = count1;
-            count1--;
+        if (modifyCount1 >= 1) {
+            setGreen(lights1, arrow1);
+            setRed(lights2, arrow2);
+            setRed(lights3, arrow3);
+            setRed(lights4, arrow4);
+            time1.textContent = modifyCount1;
+            modifyCount1--;
         } else {
-            console.log(count1);
-
+            console.log(modifyCount1);
             time1.textContent = "stop";
-            setRed(lights1);
+            extraTimeForRatio(time1, remainingTime1);
+            setRed(lights1, arrow1);
             clearInterval(timer1);
-
             timer2 = setInterval(() => {
-                if (count2 > 0) {
-                    setGreen(lights2);
-                    setRed(lights3);
-                    setRed(lights4);
-                    time2.textContent = count2;
-                    count2--;
+                if (modifyCount2 >= 1) {
+                    setGreen(lights2, arrow2);
+                    setRed(lights3, arrow3);
+                    setRed(lights4, arrow4);
+                    time2.textContent = modifyCount2;
+                    modifyCount2--;
                 } else {
-                    console.log(count2);
-
+                    console.log(modifyCount2);
                     time2.textContent = "stop";
-                    setRed(lights2);
+                    extraTimeForRatio(time2, remainingTime2);
+                    setRed(lights2, arrow2);
                     clearInterval(timer2);
-
                     timer3 = setInterval(() => {
-                        if (count3 > 0) {
-                            setGreen(lights3);
-                            setRed(lights4);
-                            time3.textContent = count3;
-                            count3--;
+                        if (modifyCount3 >= 1) {
+                            setGreen(lights3, arrow3);
+                            setRed(lights4, arrow4);
+                            time3.textContent = modifyCount3;
+                            modifyCount3--;
                         } else {
-                            console.log(count3);
-
+                            console.log(modifyCount3);
+                            extraTimeForRatio(time3, remainingTime3);
                             time3.textContent = "stop";
-                            setRed(lights3);
-
+                            setRed(lights3, arrow3);
                             clearInterval(timer3);
-
                             timer4 = setInterval(() => {
-                                if (count4 > 0) {
-                                    setGreen(lights4);
-                                    time4.textContent = count4;
-                                    count4--;
+                                if (modifyCount4 >= 1) {
+                                    setGreen(lights4, arrow4);
+                                    time4.textContent = modifyCount4;
+                                    modifyCount4--;
                                 } else {
-                                    console.log(count4);
-
+                                    console.log(modifyCount4);
+                                    extraTimeForRatio(time4, remainingTime4);
                                     time4.textContent = "stop";
-                                    setRed(lights4);
+                                    setRed(lights4, arrow4);
                                     clearInterval(timer4);
                                     if (
-                                        count1 == 0 &&
-                                        count2 == 0 &&
-                                        count3 == 0 &&
-                                        count4 == 0
+                                        modifyCount1 == 0 &&
+                                        modifyCount2 == 0 &&
+                                        modifyCount3 == 0 &&
+                                        modifyCount4 == 0
                                     ) {
-                                        timeDivider();
+                                        ratioWIseTimeDivider();
                                     }
                                 }
                             }, 1000);
